ccfeb2536293ce043a9776c7580b2e0857ead856
paingsoeko <social.kopaing@gmail.com>
2023-09-09 16:10:17 +0630

    add database file

--- database/mybank.sql
+++ database/mybank.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 19, 2023 at 06:22 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Host: localhost
+-- Generation Time: Sep 09, 2023 at 09:39 AM
+-- Server version: 8.0.34-0ubuntu0.22.04.1
+-- PHP Version: 8.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,164 +24,12 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `branch`
---
-
-CREATE TABLE `branch` (
-  `branchId` int(11) NOT NULL,
-  `branchNo` varchar(111) NOT NULL,
-  `branchName` varchar(111) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `branch`
---
-
-INSERT INTO `branch` (`branchId`, `branchNo`, `branchName`) VALUES
-(1, '100101', 'Dera Ghazi Khan'),
-(2, '100102', 'Multan');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `feedback`
---
-
-CREATE TABLE `feedback` (
-  `feedbackId` int(11) NOT NULL,
-  `message` text NOT NULL,
-  `userId` double NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `feedback`
---
-
-INSERT INTO `feedback` (`feedbackId`, `message`, `userId`, `date`) VALUES
-(1, 'This is testing message to admin or manager by fk', 1, '2017-12-15 04:30:48'),
-(3, 'This is testing message to admin or manager by fk', 2, '2017-12-15 04:30:48'),
-(4, 'this is help card for admin', 1, '2017-12-17 06:45:20'),
-(5, 'hi i am using', 1, '2023-03-18 18:03:37');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `login`
---
-
-CREATE TABLE `login` (
-  `id` int(11) NOT NULL,
-  `email` varchar(111) NOT NULL,
-  `password` varchar(111) NOT NULL,
-  `type` varchar(111) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `login`
---
-
-INSERT INTO `login` (`id`, `email`, `password`, `type`, `date`) VALUES
-(1, 'cashier@cashier.com', 'cashier', 'cashier', '2017-12-15 04:36:27'),
-(2, 'manager@manager.com', 'manager', 'manager', '2017-12-15 04:36:27'),
-(3, 'sadfas@gmail.com', 'sdfas', 'type', '2017-12-16 07:13:12'),
-(4, 'fkgeo@gmail.com', 'asdfsa', 'type', '2017-12-16 07:13:18'),
-(6, 'cashier2@cashier.com', 'cashier2', 'cashier', '2017-12-16 07:14:47');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `notice`
---
-
-CREATE TABLE `notice` (
-  `id` int(11) NOT NULL,
-  `userId` varchar(111) NOT NULL,
-  `notice` text NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `notice`
---
-
-INSERT INTO `notice` (`id`, `userId`, `notice`, `date`) VALUES
-(1, '1', 'Dear Customer! <br> OUr privacy policy is changed for account information get new prospectus from your nearest branch', '2017-12-14 13:11:46'),
-(6, '2', 'Dear Ali,<br>\r\nOur privacy policy has been changed please visit nearest <kbd> MCB </kbd> branch for new prospectus.', '2017-12-16 06:29:23');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `otheraccounts`
---
-
-CREATE TABLE `otheraccounts` (
-  `id` int(11) NOT NULL,
-  `accountNo` varchar(111) NOT NULL,
-  `bankName` varchar(111) NOT NULL,
-  `holderName` varchar(111) NOT NULL,
-  `balance` varchar(111) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `otheraccounts`
---
-
-INSERT INTO `otheraccounts` (`id`, `accountNo`, `bankName`, `holderName`, `balance`, `date`) VALUES
-(1, '12001122', 'PSK', 'Paing Soe Ko Bank', '40800', '2017-12-14 11:55:07'),
-(2, '12001123', 'GG', 'Gwat Gwat Bank', '71000', '2017-12-14 11:55:07'),
-(3, '12001124', 'HBL', 'Yousaf Khan', '71000', '2017-12-14 11:55:07');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `transaction`
---
-
-CREATE TABLE `transaction` (
-  `transactionId` int(11) NOT NULL,
-  `action` varchar(111) NOT NULL,
-  `credit` varchar(111) NOT NULL,
-  `debit` varchar(111) NOT NULL,
-  `balance` varchar(111) NOT NULL,
-  `beneId` varchar(111) NOT NULL,
-  `other` varchar(111) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data for table `transaction`
---
-
-INSERT INTO `transaction` (`transactionId`, `action`, `credit`, `debit`, `balance`, `beneId`, `other`, `userId`, `date`) VALUES
-(4, 'transfer', '', '200', '', '', '12001122', 1, '2017-12-14 12:33:40'),
-(5, 'transfer', '', '200', '', '', '10054777', 1, '2017-12-14 12:56:48'),
-(6, 'transfer', '', '333', '', '', '10054777', 1, '2017-12-14 12:57:20'),
-(7, 'transfer', '', '222', '', '', '10054777', 1, '2017-12-14 12:58:19'),
-(8, 'transfer', '', '333', '', '', '10054777', 1, '2017-12-14 13:00:23'),
-(16, 'withdraw', '', '100', '', '', '23423', 1, '2017-12-15 08:31:59'),
-(17, 'deposit', '1200', '', '', '', '234232', 1, '2017-12-15 08:32:17'),
-(18, 'transfer', '', '467', '', '', '12001122', 1, '2017-12-17 06:44:48'),
-(22, 'deposit', '1200', '', '', '', '32424', 2, '2017-12-17 06:56:29'),
-(23, 'withdraw', '', '12', '', '', '23423', 2, '2017-12-17 06:59:02'),
-(24, 'deposit', '12', '', '', '', '21321', 2, '2017-12-17 06:59:20'),
-(25, 'transfer', '', '1200', '', '', '10054777', 1, '2017-12-17 07:01:37'),
-(26, 'deposit', '600', '', '', '', '342342', 2, '2017-12-17 07:04:39'),
-(27, 'withdraw', '', '1012', '', '', '23423', 2, '2017-12-17 07:04:58'),
-(28, 'transfer', '', '100', '', '', '10054777', 1, '2023-03-19 04:37:36'),
-(29, 'transfer', '', '700', '', '', '10054777', 1, '2023-03-19 04:38:45');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `useraccounts`
 --
 
 CREATE TABLE `useraccounts` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
+  `is_first_login` tinyint(1) NOT NULL DEFAULT '1',
   `email` text NOT NULL,
   `password` text NOT NULL,
   `name` varchar(111) NOT NULL,
@@ -194,58 +42,22 @@ CREATE TABLE `useraccounts` (
   `accountNo` varchar(111) NOT NULL,
   `branch` varchar(111) NOT NULL,
   `accountType` varchar(111) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `useraccounts`
 --
 
-INSERT INTO `useraccounts` (`id`, `email`, `password`, `name`, `balance`, `cnic`, `number`, `city`, `address`, `source`, `accountNo`, `branch`, `accountType`, `date`) VALUES
-(1, 'some@gmail.com', 'some', 'Kopaing', '9000', '3210375555426', '03356910260', 'Islamabad', 'Some where in isb', 'Programmer', '1005469', '1', 'Current', '2017-12-14 05:50:06'),
-(2, 'some2@gmail.com', 'some2', 'Gwat Gwat', '16800', '3210375555343', '03356910260', 'Islamabad', 'Some where in isb', 'Programmer', '10054777', '1', 'Saving', '2017-12-14 04:50:06'),
-(6, 'realx4rd@gmail.com', 'afsdfasd', 'Maung Htay', '234234', '3240338834902', '03356910260', 'Taunsa', 'Dera Ghazi Khan', 'Govt. job', '1513410739', '1', 'saving', '2017-12-16 07:52:40'),
-(7, 'realx4rd@gmail.com', 'safsadf', 'Min Khant', '12121', '3240338834902', '03356910260', 'Taunsa', 'Dera Ghazi Khan', 'Govt. job', '1513410837', '1', 'current', '2017-12-16 07:54:18');
+INSERT INTO `useraccounts` (`id`, `is_first_login`, `email`, `password`, `name`, `balance`, `cnic`, `number`, `city`, `address`, `source`, `accountNo`, `branch`, `accountType`, `date`) VALUES
+(2, 1, 'some2@gmail.com', '$2y$10$YDdTFL9Vmsu2Xymrud0iwOM/Djvpdp5HOQCzZ.AmOyhMtXBjEMuXW', 'Gwat Gwat', '16800', '3210375555343', '03356910260', 'Islamabad', 'Some where in isb', 'Programmer', '10054777', '1', 'Saving', '2017-12-14 04:50:06'),
+(6, 1, 'realx4rd@gmail.com', '$2y$10$fcBn6hqFf8F/.jCY7iBiTetaeowrtRxS.HYgGHnlvYuedJOzT7DSS', 'Maung Htay', '234234', '3240338834902', '03356910260', 'Taunsa', 'Dera Ghazi Khan', 'Govt. job', '1513410739', '1', 'saving', '2017-12-16 07:52:40'),
+(7, 1, 'minkhant@gmail.com', '$2y$10$cQyk2QAdxWGhCd9gDV0VAuU2kxeh2EKzpTwfjDO7r0fhEj5myRgXG', 'Min Khant', '12121', '3240338834902', '03356910260', 'Taunsa', 'Dera Ghazi Khan', 'Govt. job', '1513410837', '1', 'current', '2017-12-16 07:54:18'),
+(10, 0, 'some@gmail.com', '$2y$10$3C87ECmClfDMgIWz4DIL4uHL.D3JkQZINppu2yhWiqzzoXf.jKeMq', 'Kopaing', '9000', '3210375555426', '03356910260', 'Islamabad', 'Some where in isb', 'Programmer', '1005469', '1', 'Current', '2017-12-14 05:50:06');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `branch`
---
-ALTER TABLE `branch`
-  ADD PRIMARY KEY (`branchId`);
-
---
--- Indexes for table `feedback`
---
-ALTER TABLE `feedback`
-  ADD PRIMARY KEY (`feedbackId`);
-
---
--- Indexes for table `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `notice`
---
-ALTER TABLE `notice`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `otheraccounts`
---
-ALTER TABLE `otheraccounts`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `transaction`
---
-ALTER TABLE `transaction`
-  ADD PRIMARY KEY (`transactionId`);
 
 --
 -- Indexes for table `useraccounts`
@@ -258,46 +70,10 @@ ALTER TABLE `useraccounts`
 --
 
 --
--- AUTO_INCREMENT for table `branch`
---
-ALTER TABLE `branch`
-  MODIFY `branchId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `feedback`
---
-ALTER TABLE `feedback`
-  MODIFY `feedbackId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `login`
---
-ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `notice`
---
-ALTER TABLE `notice`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `otheraccounts`
---
-ALTER TABLE `otheraccounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `transaction`
---
-ALTER TABLE `transaction`
-  MODIFY `transactionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
-
---
 -- AUTO_INCREMENT for table `useraccounts`
 --
 ALTER TABLE `useraccounts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
